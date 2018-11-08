49713ba781914b3fb895b1070ef4f52e327354b4
Maylem Gonzalez <maylem.gonzalez@gmail.com>
2018-11-08 03:00:32 -0500

    Fixed port bug in run.py

--- app/run.py
+++ app/run.py
@@ -1,3 +1,5 @@
+import os
+
 import connexion
 from flask import render_template
 
@@ -12,4 +14,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
