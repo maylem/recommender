ffbc6796b385607b9527d38cf8253d8a6e31d419
Maylem Gonzalez <maylem.gonzalez@gmail.com>
2018-11-15 12:24:13 -0500

    Update run logic to get user matrix and build recommender

--- app/run.py
+++ app/run.py
@@ -3,8 +3,10 @@ import os
 import connexion
 from flask import render_template
 
+from build_user_matrix import get_user_data
 from db.create_db import create_db
 from db.table_config import DATA_FILE_PATHS, TABLE_DESC
+from user_recommender import build_recommender
 
 app = connexion.App(__name__, specification_dir="./")
 
@@ -17,7 +19,10 @@ def home():
 
 
 if __name__ == '__main__':
-    create_db('db/ps.sqlite', file_paths=DATA_FILE_PATHS, table_desc=TABLE_DESC)
+    db_conn = create_db('db/ps.sqlite', file_paths=DATA_FILE_PATHS, table_desc=TABLE_DESC)
+
+    users = get_user_data(db_conn)
+    build_recommender(users)
 
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
